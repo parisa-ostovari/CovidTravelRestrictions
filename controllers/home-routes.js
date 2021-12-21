e5a63c69a9620eee1bcb364a6b278c187195d6b7
Juwon Oh <edmaster41@gmail.com>
2021-12-20 21:59:17 -0800

    Routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,38 +17,33 @@ catch(err){
 }
 })
 
-router.get('/get', async (req, res) => {
+router.get('/coolkids', async (req, res) => {
   try{
-    const dbUserdata = await User.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['id', 'description'],
-        },
-      ],
-  })
-
-    const userData = dbUserdata.map((users) => {
-      users.get({plain:true})
+    const dbUserdata = await User.findAll()
+    console.log(dbUserdata)
+    const userData = dbUserdata.map((user) => {
+      return user.get({plain:true})
       
     })
-    return res.json(userData)
+    res.json(userData)
     console.log("shit")
   }
   catch(err){
-    res.status(500).json
+    res.status(500).json(err)
   }
 })
 //db
-router.get('/get', async (req, res) => {
-  try{
+// router.get('/get', async (req, res) => {
+//   try{
    
   
-  }
-  catch(err){
-    res.status(500).json
-  }
-})
+//   }
+//   catch(err){
+//     res.status(500).json
+//   }
+// })
+//making third party api calls with proxy server in express.js
+
 // Login route
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
