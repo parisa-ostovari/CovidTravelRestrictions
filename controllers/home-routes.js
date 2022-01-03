1e1eaa07269fe53f0542eab73a8e84dcd8aa77ec
Parisa Ostovari <parisaostovari@gmail.com>
2022-01-03 11:00:11 -0800

    404 not occuring now

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,95 @@
 const router = require('express').Router();
 const { User } = require('../models/');
 
-// -- Code from 6 to 25 is correct -- //
+
+
+
+// Home page route
+router.get('/', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('homepage');
+});
+
+
+// Saved-destination route
+router.get('/saved', async (req, res) => {
+  try {
+    const dbUserData = await User.findAll()
+    console.log(dbUserData)
+    const userData = dbUserData.map((user) => {
+      return user.get({ plain: true })
+    })
+    res.render('saved', { userData })
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+
+router.post('/create', async (req, res) => {
+  try {
+    const dbUser = await User.create({
+      email: req.body.email,
+      password: req.body.password
+    })
+    res.json(dbUser)
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.get('/get', async (req, res) => {
+  try {
+    const dbUserData = await User.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['id', 'description'],
+        },
+      ],
+    })
+
+    const userData = dbUserData.map((users) => {
+      users.get({ plain: true })
+
+    })
+    return res.json(userData)
+    console.log("shit")
+  }
+  catch (err) {
+    res.status(500).json
+  }
+})
+
+router.get('/get', async (req, res) => {
+  try {
+    const db = mysql.createConnection(
+      {
+        host: '127.0.0.1',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: '',
+        database: 'courses_db'
+      },
+      console.log(`Connected to the courses_db database.`)
+    );
+    db.query('SELECT * FROM course_names', function (err, results) {
+      console.log(results);
+    });
+
+  }
+  catch (err) {
+    res.status(500).json
+  }
+})
+
+// -- Code from 94 to 113 is correct -- //
 // Login route
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
@@ -21,92 +109,3 @@ router.get('/signup', (req, res) => {
 });
 
 module.exports = router;
-
-
-
-// // Home page route
-// router.get('/', (req, res) => {
-//   if (req.session.loggedIn) {
-//     res.redirect('/');
-//     return;
-//   }
-//   res.render('homepage');
-// });
-
-
-// // Saved-destination route
-// router.get('/saved', async (req, res) => {
-//   try{
-//     const dbUserData = await User.findAll()
-//     console.log(dbUserData)
-//     const userData = dbUserData.map((user) => {
-//       return user.get({plain:true})
-//     })
-//   res.render('saved', {userData})
-//   }
-//   catch(err){
-//     res.status(500).json(err)
-//   }
-// })
-
-
-// router.post('/create', async (req, res) => {
-//   try{
-//   const dbUser = await User.create({
-//     email: req.body.email,
-//     password: req.body.password
-//   })
-//   res.json(dbUser)
-// }
-// catch(err){
-//   res.status(500).json(err)
-// }
-// })
-
-// router.get('/get', async (req, res) => {
-//   try{
-//     const dbUserData = await User.findAll({
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['id', 'description'],
-//         },
-//       ],
-//   })
-
-//     const userData = dbUserData.map((users) => {
-//       users.get({plain:true})
-      
-//     })
-//     return res.json(userData)
-//     console.log("shit")
-//   }
-//   catch(err){
-//     res.status(500).json
-//   }
-// })
-
-
-//db
-// router.get('/get', async (req, res) => {
-//   try{
-//     const db = mysql.createConnection(
-//       {
-//         host: '127.0.0.1',
-//         // MySQL username,
-//         user: 'root',
-//         // MySQL password
-//         password: '',
-//         database: 'courses_db'
-//       },
-//       console.log(`Connected to the courses_db database.`)
-//     );
-//     db.query('SELECT * FROM course_names', function (err, results) {
-//       console.log(results);
-//     });
-  
-//   }
-//   catch(err){
-//     res.status(500).json
-//   }
-// })
