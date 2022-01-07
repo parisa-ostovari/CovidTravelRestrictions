7625ad1da0015ac078f012bb8315587413fce9ff
Caroline Pastorelli <cpastorelli@arcanabrewing.com>
2022-01-06 19:24:28 -0800

    deleting unnecessary files

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,2 @@
 DROP DATABASE IF EXISTS covidUser_db;
 CREATE DATABASE covidUser_db;
-
-DROP DATABASE IF EXISTS countryRestrictions_db;
-CREATE DATABASE countryRestrictions_db;
--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 
 const User = require('./User');
-const Country = require('./Country');
 
 
-module.exports = { User, Country };
+
+module.exports = { User };
 
